524148d7b1c6139a0a3eecac2021c0ecc5875c82
studtrantrongtien <trantrongtien9197@gmail.com>
2020-03-18 15:52:08 +0300

    category suite

--- src/test/java/lab2/SuiteTestLab2.java
+++ src/test/java/lab2/SuiteTestLab2.java
@@ -1,11 +1,11 @@
 package lab2;
 
+import org.junit.experimental.categories.Categories;
 import org.junit.experimental.categories.Categories.IncludeCategory;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
+@RunWith(Categories.class)
 @IncludeCategory(SmokeTests.class)
 @SuiteClasses(TestLab2.class)
 public class SuiteTestLab2 {
--- src/test/java/lab2/TestLab2.java
+++ src/test/java/lab2/TestLab2.java
@@ -19,7 +19,6 @@ import lab2.pages.Homepage;
 import lab2.pages.LoginPage;
 import lab2.pages.ProfilePage;
 
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestLab2 {
 	static WebDriver 	webDriver;
@@ -39,7 +38,6 @@ public class TestLab2 {
         profile = new ProfilePage(webDriver);
 	}
 
-	public class TestLogin{
 		@Category(SmokeTests.class)
 		@Test
 		public void testA_Login() {
@@ -47,47 +45,46 @@ public class TestLab2 {
 			login.login("trongtien.tran.18400", "53230eb89891d8ebe75c0d296841789fae3b7bb8");
 			login.verifyIfLoginSuccess("Trongtien Tran");	
 		}
-	}
+		
+		@Category(SmokeTests.class)
+		@Test
+		public void testF_LoginFailed() {
+			login.logout();
+			webDriver.navigate().to("https://www.facebook.com/login.php");
+			login.login("trongtien.tran.18400", "d296841789fae3b7bb8");
+			login.verifyIfLoginFailed("Log in as Trongtien Tran");
+		}
 	
 	
-	@Test
-	public void testB_HomeSearch() {
-		webDriver.navigate().to("https://www.facebook.com");
-		home.searchText("spbstu");
-		home.verifySearch("Peter the Great St.Petersburg Polytechnic University");
-	}
+		@Test
+		public void testB_HomeSearch() {
+			webDriver.navigate().to("https://www.facebook.com");
+			home.searchText("spbstu");
+			home.verifySearch("Peter the Great St.Petersburg Polytechnic University");
+		}
+	
+		@Category(SmokeTests.class)
+		@Test
+		public void testC_HomeTitle() {
+			webDriver.navigate().to("https://www.facebook.com");
+			System.out.println(webDriver.getTitle());
+			assertEquals("Facebook", webDriver.getTitle());
+		}
+	
+		@Category(SmokeTests.class)
+		@Test
+		public void testD_Like() {
+			webDriver.navigate().to("https://www.facebook.com/trongtien.tran.18400");
+			profile.clickLike();
+			profile.verifyLike();
+		}
+		
+		@Test
+		public void testE_BornDay() {
+			webDriver.navigate().to("https://www.facebook.com/trongtien.tran.18400");
+			profile.verifyBornDay("Born on 1 January 1997");
+		}
 
-	@Category(SmokeTests.class)
-	@Test
-	public void testC_HomeTitle() {
-		webDriver.navigate().to("https://www.facebook.com");
-		System.out.println(webDriver.getTitle());
-		assertEquals("Facebook", webDriver.getTitle());
-	}
-	
-	@Category(SmokeTests.class)
-	@Test
-	public void testD_Like() {
-		webDriver.navigate().to("https://www.facebook.com/trongtien.tran.18400");
-		profile.clickLike();
-		profile.verifyLike();
-	}
-	
-	@Test
-	public void testE_BornDay() {
-		webDriver.navigate().to("https://www.facebook.com/trongtien.tran.18400");
-		profile.verifyBornDay("Born on 1 January 1997");
-	}
-
-	@Category(SmokeTests.class)
-	@Test
-	public void testF_LoginFailed() {
-		login.logout();
-		webDriver.navigate().to("https://www.facebook.com/login.php");
-		login.login("trongtien.tran.18400", "d296841789fae3b7bb8");
-		login.verifyIfLoginFailed("Log in as Trongtien Tran");
-	}
-	
 	@AfterClass
     public static void shutdownDriver() {
         if (webDriver != null) {
